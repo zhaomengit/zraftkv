f9a398404cee7508630a74f13c1cccfe2b070d78
zhaomengit <zhaomengit@163.com>
2017-04-18 18:22:04 +0800

    增加其他函数

--- httpapi.go
+++ httpapi.go
@@ -1,48 +1,112 @@
 package main
 
 import (
-    "net/http"
-    "strconv"
-    "log"
+	"github.com/coreos/etcd/raft/raftpb"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 type httpKVAPI struct {
-
+	store       *kvstore
+	confChangeC chan<- raftpb.ConfChange // 发送raft confchange channel
 }
 
 // 实现的Handler接口
 func (h *httpKVAPI) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    key := r.RequestURI
-    switch {
-    case r.Method == "PUT":
-        w.WriteHeader(http.StatusNoContent)
-    case r.Method == "POST":
-        w.WriteHeader(http.StatusNoContent)
-    case r.Method == "GET":
-        w.Write([]byte(key))
-    case r.Method == "DELETE":
-        w.WriteHeader(http.StatusNoContent)
-    default:
-        // 设置Head头信息
-        w.Header().Set("Allow", "PUT")
-        w.Header().Add("Allow", "GET")
-        w.Header().Add("Allow", "POST")
-        w.Header().Add("Allow", "DELETE")
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    }
+	// 获取key
+	key := r.RequestURI
+	switch {
+	case r.Method == "PUT":
+		v, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Failed to read on PUT (%v)\n", err)
+			http.Error(w, "Failed on PUT", http.StatusBadRequest)
+			return
+		}
+
+		// 向channel
+		h.store.Propose(key, string(v))
+		w.WriteHeader(http.StatusNoContent)
+
+	case r.Method == "POST": // 增加节点
+
+		url, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			log.Printf("Failed to read on POST (%v)\n", err)
+			http.Error(w, "Failed on POST", http.StatusBadRequest)
+			return
+		}
+
+		nodeId, err := strconv.ParseUint(key[1:], 0, 64)
+		if err != nil {
+			log.Printf("Failed to convert ID for conf change (%v)\n", err)
+			http.Error(w, "Failed on POST", http.StatusBadRequest)
+			return
+		}
+
+		cc := raftpb.ConfChange{
+			Type:    raftpb.ConfChangeAddNode, // 增加节点
+			NodeID:  nodeId,
+			Context: url,
+		}
+		h.confChangeC <- cc
+
+		w.WriteHeader(http.StatusNoContent)
+	case r.Method == "GET":
+
+		// get从store的map中查找key
+		if v, ok := h.store.Lookup(key); ok {
+			w.Write([]byte(v))
+		} else {
+			http.Error(w, "Failed to GET", http.StatusNotFound)
+		}
+
+	case r.Method == "DELETE": // 删除节点
+
+		// 取nodeid
+		nodeId, err := strconv.ParseUint(key[1:], 0, 64)
+
+		if err != nil {
+			log.Printf("Failed to convert ID for conf change (%v)\n %d", err, nodeId)
+			http.Error(w, "Failed on DELETE", http.StatusBadRequest)
+			return
+		}
+
+		// 构造raft confchange对象
+		cc := raftpb.ConfChange{
+			Type:   raftpb.ConfChangeRemoveNode, // 删除节点
+			NodeID: nodeId,
+		}
+		// 发送
+		h.confChangeC <- cc
+		w.WriteHeader(http.StatusNoContent)
+
+	default:
+		// 设置Head头信息
+		w.Header().Set("Allow", "PUT")
+		w.Header().Add("Allow", "GET")
+		w.Header().Add("Allow", "POST")
+		w.Header().Add("Allow", "DELETE")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 
 }
 
-func serveHttpKVAPI(kvport int)  {
-    srv := http.Server{
-        Addr: ":" + strconv.Itoa(kvport),
-        Handler: &httpKVAPI{
+// 开启api REST 服务
+func serveHttpKVAPI(kv *kvstore, port int, confChangeC chan<- raftpb.ConfChange, errorC <-chan error) {
+	srv := http.Server{
+		Addr: ":" + strconv.Itoa(port),
+		Handler: &httpKVAPI{
+			store:       kv,
+			confChangeC: confChangeC,
+		},
+	}
 
-        },
-    }
-
-    if err := srv.ListenAndServe(); err != nil {
-        log.Fatal(err)
-    }
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "flag"
     "fmt"
+    "github.com/coreos/etcd/raft/raftpb"
+    "strings"
 )
 
 func main()  {
@@ -14,12 +16,28 @@ func main()  {
     kvport := flag.Int("port", 9191, "RESTFul API访问端口")
     // 是否是加入集群
     join := flag.Bool("join", false, "是否加入一个新的集群")
-
     flag.Parse()
-
     fmt.Print(*cluster, *id, *kvport, *join)
 
-    //开启RESTFull API Server
-    serveHttpKVAPI(*kvport)
+    // 创建发送channel
+    proposeC := make(chan string)
+    defer close(proposeC)
+
+    // 创建ConfChange channel
+    confChangeC := make(chan raftpb.ConfChange)
+    defer close(confChangeC)
+
+    var kvs *kvstore
+
+    // 包装一下函数,包外访问不到
+    getSnapshot := func() ([]byte, error) { return kvs.getSnapshot() }
+
+    // 创建raft节点
+    commitC, errorC, snapshotterReady := newRaftNode(*id, strings.Split(*cluster, ","), *join, getSnapshot, proposeC, confChangeC)
+
+    kvs = newKVStore(<-snapshotterReady, proposeC, commitC, errorC)
+
+    // 启动动http api服务器,处理发送到的raft请求
+    serveHttpKVAPI(kvs, *kvport, confChangeC, errorC)
 }
 
